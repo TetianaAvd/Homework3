35592b7f9ef36afaaccebaea0ef3331ebe716e8f
TetianaAvd <shyshyavd@gmail.com>
2023-06-27 01:55:06 +0200

    init commit

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("student’s grade: ");
-    int grade = scanner.nextInt();
+        int grade = scanner.nextInt();
 
-        if(grade >= 90){
+        if (grade >= 91 && grade <= 100) {
+            System.out.println("A");
+        } else if (grade >= 81 && grade <= 90) {
             System.out.println("B");
-        } else if(grade >= 60){
+        } else if (grade >= 71 && grade <= 80) {
+            System.out.println("C");
+        } else if (grade >= 61 && grade <= 70) {
+            System.out.println("D");
+        } else if (grade >= 0 && grade <= 60) {
             System.out.println("E");
         }
     }
